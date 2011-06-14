ad304cb7e78f3ef21ec7356b8747e1a0f6e9d549
r-yoshimura <r-yoshimura@f9e7563d-c07c-ba4f-b3db-62cb7e23badb>
2011-06-14 12:23:47 +0000

    mod peeling, works so so.
    git-svn-id: http://localhost/svn/repository@27 f9e7563d-c07c-ba4f-b3db-62cb7e23badb

--- OutofRangePeel/OutofrangePeel/OutofrangePeel.h
+++ OutofRangePeel/OutofrangePeel/OutofrangePeel.h
@@ -12,6 +12,10 @@
 
 #include "QhullCalc.h"
 
+#include <boost/unordered_set.hpp>
+
+#include <fstream>
+
 
 
 class Stroke
@@ -132,19 +136,26 @@ public:
 
 class Peel
 {
-private:
+public:
 
 	typedef Rydot::Vector3f	Vector3;
-
-	std::vector<Vector3> points;
-	std::vector<std::vector<int>> cutter;
-	std::vector<std::vector<int>> mesh;
-
-	std::vector<Vector3> splpoints;
 
 	struct Face
 	{
 		std::vector<int> coedgeIndices;
+
+		// sub
+		std::vector<int> pointIndices;
+		std::vector<int> splpointIndices;
+
+		bool HasPoint(int pi)const
+		{
+			for(size_t i=0;i<pointIndices.size();++i)
+			{
+				if(pointIndices[i]==pi)return true;
+			}
+			return false;
+		}
 	};
 
 	struct Coedge
@@ -152,8 +163,9 @@ private:
 		int faceIndex;
 		int edgeIndex;
 		std::vector<int> pointIndices;
+		int sense;
 
-		bool IsSibling(const Coedge &c)
+		bool IsSibling(const Coedge &c)const
 		{
 			if(pointIndices[0]==c.pointIndices[0] && pointIndices[1]==c.pointIndices[1] )return true;
 			if(pointIndices[0]==c.pointIndices[1] && pointIndices[1]==c.pointIndices[0] )return true;
@@ -168,14 +180,102 @@ private:
 		bool split;
 	};
 
+private:
+
+	std::vector<Vector3> points;
+	std::vector<std::vector<int>> cutter;
+	std::vector<std::vector<int>> mesh;
+
+	std::vector<Vector3> splpoints;
+
 	std::vector<Face> faces;
 	std::vector<Coedge> coedges;
 	std::vector<Edge> edges;
+
+	// returns sibling coedge of coedges[ce]
+	int FindSiblingCoedge(int ce)const
+	{
+		if(ce<0 || ce>=coedges.size())return -1;
+		const Coedge &c=coedges[ce];
+		if(c.edgeIndex<0)return -1;
+		const Edge &e=edges[c.edgeIndex];
+		if(e.coedgeIndices.size()<2)return -1;
+		return (e.coedgeIndices[0]==ce)?e.coedgeIndices[1]:e.coedgeIndices[0];
+	}
+
+	std::vector<int> FindAdjacentFaces(int fi)const
+	{
+		if(fi<0 || fi>=faces.size())return std::vector<int>();
+
+		const Face &f=faces[fi];
+		std::vector<int> r;
+		for(size_t i=0;i<f.coedgeIndices.size();++i)
+		{
+			int sib=FindSiblingCoedge(f.coedgeIndices[i]);
+			if(sib>=0)
+			{
+				r.push_back(coedges[sib].faceIndex);
+			}
+		}
+		return r;
+	}
+
+	// returns adjacent faces from faces[fi] where belongs to points[pi]
+	std::vector<int> PointFacePie(int pi, int fi)const
+	{
+		if(pi<0 || pi>=points.size())return std::vector<int>();
+		if(fi<0 || fi>=faces.size())return std::vector<int>();
+
+		std::vector<int> working;
+		boost::unordered_set<int> used;
+		working.push_back(fi);
+		used.insert(fi);
+		while(!working.empty())
+		{
+			std::vector<int> newwork;
+			for(int i=0;i<working.size();++i)
+			{
+				std::vector<int> adj=FindAdjacentFaces(working[i]);
+				for(int j=0;j<adj.size();++j)
+				{
+					if((used.find(adj[j])==used.end()) && faces[adj[j]].HasPoint(pi))
+					{
+						newwork.push_back(adj[j]);
+						used.insert(adj[j]);
+					}
+				}
+			}
+			working=newwork;
+		}
+		std::vector<int> res;
+		std::copy(used.begin(),used.end(),back_inserter(res));
+		return res;
+	}
+
+	bool ReassignEdges(int pi, int fi, int pi2)
+	{
+		if(pi<0 || pi>=points.size())return false;
+		if(fi<0 || fi>=faces.size())return false;
+
+		const Face &f=faces[fi];
+		for(size_t i=0;i<f.coedgeIndices.size();++i)
+		{
+			int eidx=coedges[f.coedgeIndices[i]].edgeIndex;
+			Edge &e=edges[eidx];
+			if(e.splPointIndices[0]==pi)e.splPointIndices[0]=pi2;
+			if(e.splPointIndices[1]==pi)e.splPointIndices[1]=pi2;
+		}
+		return true;
+	}
 
 public:
 	Peel()
 	{
 	}
+
+
+	const std::vector<Face> &GetFaces(){return faces;}
+	const std::vector<Vector3> &GetSplPoints(){return splpoints;}
 
 
 
@@ -214,6 +314,10 @@ public:
 				c.pointIndices.push_back(mesh[i][k]);
 				c.pointIndices.push_back(mesh[i][(k+1)%3]);
 			}
+			for(int k=0;k<3;++k)
+			{
+				faces[i].pointIndices.push_back(mesh[i][k]);
+			}
 		}
 
 		// find sibling
@@ -231,8 +335,11 @@ public:
 					e.coedgeIndices.push_back(std::min(i,k));
 					e.coedgeIndices.push_back(std::max(i,k));
 					e.split=false;
+					e.splPointIndices=coedges[i].pointIndices;
 					coedges[i].edgeIndex=eidx;
 					coedges[k].edgeIndex=eidx;
+					coedges[i].sense=true;
+					coedges[k].sense=false;
 				}
 			}
 		}
@@ -252,35 +359,104 @@ public:
 					int eidx=coedges[k].edgeIndex;
 					if(eidx>=0)
 					{
-						Edge &e=edges[eidx];
-						coedges[e.coedgeIndices[0]].edgeIndex=-1;
-						coedges[e.coedgeIndices[1]].edgeIndex=-1;
-						e.split=true;
+						Edge &e0=edges[eidx];
+						if(e0.split)continue;
+						coedges[e0.coedgeIndices[1]].edgeIndex=edges.size();
+						Edge e1=e0;
+						e1.coedgeIndices.clear();
+						e1.coedgeIndices.push_back(e0.coedgeIndices[1]);
+						e0.coedgeIndices.pop_back();
+						e0.split=true;
+						e1.split=true;
+						edges.push_back(e1);
 					}
 					break;
 				}
 			}
 		}
 
-		// assign split points
-		splpoints.clear();
-		size_t ne=edges.size();
-		for(size_t i=0;i<ne;++i)
+		// [point index][]=>face index
+		std::vector<std::vector<int>> pifi(points.size());
+		for(size_t i=0;i<faces.size();++i)
 		{
-			if(edges[i].split)
+			const Face &f=faces[i];
+			for(size_t j=0;j<f.pointIndices.size();++j)
 			{
-				edges.push_back(Edge());
-				Edge &ei=edges[i];
-				
-
-
+				pifi[f.pointIndices[j]].push_back(i);
 			}
-
-
 		}
 
-		
+		// cluster
+		// [point index][][]=>face index
+		std::vector<std::vector<std::vector<int>>> picluster(points.size());
+		for(size_t i=0;i<pifi.size();++i)
+		{
+			const std::vector<int> &fcs=pifi[i];
+			boost::unordered_set<int> used;
+			for(size_t j=0;j<fcs.size();++j)
+			{
+				if(used.find(fcs[j])!=used.end())continue;
+				std::vector<int> pie=PointFacePie(i, fcs[j]);
+				picluster[i].push_back(pie);
+				for(size_t k=0;k<pie.size();++k)
+				{
+					used.insert(pie[k]);
+				}
+			}
+		}
+
+		// assign split points
+		splpoints=points;
+		for(size_t i=0;i<picluster.size();++i)
+		{
+			if(picluster[i].size()>1)
+			{
+				for(size_t k=1;k<picluster[i].size();++k)
+				{
+					int splidx=splpoints.size();
+					splpoints.push_back(points[i]);
+					for(size_t a=0;a<picluster[i][k].size();++a)
+					{
+						ReassignEdges(i, picluster[i][k][a], splidx);
+					}
+				}
+			}
+		}
+
+		// assign face point index
+		for(size_t i=0;i<faces.size();++i)
+		{
+			Face &f=faces[i];
+			for(size_t j=0;j<f.coedgeIndices.size();++j)
+			{
+				const Coedge &ce=coedges[f.coedgeIndices[j]];
+				int eidx=ce.edgeIndex;
+				if(ce.sense)
+				{
+					f.splpointIndices.push_back(edges[eidx].splPointIndices[0]);
+				}
+				else
+				{
+					f.splpointIndices.push_back(edges[eidx].splPointIndices[1]);
+				}
+			}
+		}
+
 		return false;
+	}
+
+
+	//debug
+	bool OutDot()
+	{
+		std::ofstream f("c:\\a.dot");
+		f<<"graph sample{\n";
+		for(size_t i=0;i<edges.size();++i)
+		{
+			f<<"  p"<<edges[i].splPointIndices[0]<<" -- p"<<edges[i].splPointIndices[1]<<"\n";
+		}
+		f<<"}\n";
+		return true;
 	}
 
 
@@ -333,6 +509,9 @@ class OrangePeel:public IGLEvents
 	std::vector<Stroke> strokes;
 	
 	std::vector<Vector3> control;
+
+
+	Peel pl;
 
 
 public:
@@ -459,9 +638,9 @@ private:
 		double pi=4.0*atan(1.0);
 		double R=10;
 		Stroke s;
-		for(int i=0;i<400;++i)
+		for(int i=0;i<100;++i)
 		{
-			double t=(double)i/(400.0-1.0);
+			double t=(double)i/(100.0-1.0);
 			double z=cos(pi*t);
 			double ss=sin(pi*t);
 			double x=cos(2.0*pi*R*t)*ss;
@@ -489,12 +668,21 @@ private:
 		//vtx.push_back(Rydot::Vector3d(-1,-1,1).Norm());
 		//vtx.push_back(Rydot::Vector3d(1,-1,-1).Norm());
 		//vtx.push_back(Rydot::Vector3d(-1,1,-1).Norm());
+		std::vector<std::vector<int>> cutter;
 		for(size_t i=0;i<strokes.size();++i)
 		{
 			const std::vector<Vector3> &s=strokes[i].Get();
+			int vtxidx=vtx.size();
 			for(size_t k=0;k<s.size();++k)
 			{
 				vtx.push_back(Rydot::Vector3d(s[k]));
+			}
+			for(size_t k=0;k+1<s.size();++k)
+			{
+				std::vector<int> cut;
+				cut.push_back(vtxidx+k);
+				cut.push_back(vtxidx+k+1);
+				cutter.push_back(cut);
 			}
 		}
 
@@ -510,22 +698,31 @@ private:
 		}
 
 		// find matching
-		std::vector<int> match(qhp.size());
-		for(size_t i=0;i<qhp.size();++i)
-		{
-			double nearest=1e10;
-			int idx=0;
-			for(size_t k=0;k<vtx.size();++k)
-			{
-				double d2=qhp[i].Dist2(vtx[k]);
-				if(d2<nearest)
-				{
-					nearest=d2;
-					idx=k;
-				}
-			}
-			match[i]=idx;
-		}
+		//std::vector<int> match(qhp.size());
+		//for(size_t i=0;i<qhp.size();++i)
+		//{
+		//	double nearest=1e10;
+		//	int idx=0;
+		//	for(size_t k=0;k<vtx.size();++k)
+		//	{
+		//		double d2=qhp[i].Dist2(vtx[k]);
+		//		if(d2<nearest)
+		//		{
+		//			nearest=d2;
+		//			idx=k;
+		//		}
+		//	}
+		//	match[i]=idx;
+		//}
+
+		//Peel pl;
+		pl.SetupMesh(qhfcs);
+		pl.SetupCutter(cutter);
+		pl.SetupPoints(qhpts);
+		pl.CreateSplit();
+
+		pl.OutDot();
+
 
 #ifdef _WIN32
 		double v=0;
@@ -651,17 +848,32 @@ private:
 		float emiorange[]={0.3f,0.2f,0.1f,1};
 		glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, emiorange);
 
-		for(size_t i=0;i<m.faces.size();++i)
+		//for(size_t i=0;i<m.faces.size();++i)
+		//{
+		//	for(size_t v=0;v<3;++v)
+		//	{
+		//		size_t p=m.faces[i].vertex[v];
+		//		glNormal3fv(&(m.normals[p].x));
+		//		glVertex3fv(&(m.vertex[p].x));
+		//	}
+		//}
+		glEnd();
+		glDisable(GL_LIGHTING);
+
+		const std::vector<Peel::Face> &fcs=pl.GetFaces();
+		const std::vector<Vector3> &pts=pl.GetSplPoints();
+		glBegin(GL_TRIANGLES);
+		for(size_t i=0;i<fcs.size();++i)
 		{
 			for(size_t v=0;v<3;++v)
 			{
-				size_t p=m.faces[i].vertex[v];
-				glNormal3fv(&(m.normals[p].x));
-				glVertex3fv(&(m.vertex[p].x));
+				glColor3f((i%10)/10.0,((i/10)%10)/10.0,0);
+				int idx=fcs[i].splpointIndices[2-v];
+				Vector3 p=pts[idx]+Vector3(0.0,0.0,idx*0.001);
+				glVertex3fv(&(p.x));
 			}
 		}
 		glEnd();
-		glDisable(GL_LIGHTING);
 
 		{
 			std::pair<Vector3, Vector3> r = v.Ray(Vector2(mousex+1, mousey+1));
